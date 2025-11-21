8188715395cca311fc3ac32faf24de1420c49973
mafev4 <maria.vera7@est.uexternado.edu.co>
2025-11-21 15:31:36 +0000

    ok

--- .pandas/figuras.py
+++ .pandas/figuras.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 
-prom_cp1 = df.filter(regex="^CP1$").iloc[:, 0].mean()
-prom_nf  = df.filter(regex="^NF$").iloc[:, 0].mean()
+prom_cp1 = df.filter(regex="CP1").iloc[:, 0].mean()
+prom_nf  = df.filter(regex="NF").iloc[:, 0].mean()
 prom_cp2 = df["CP2"].mean()
 prom_cp3 = df["CP3"].mean()
 plt.figure(figsize=(6,4))
@@ -12,5 +12,5 @@ plt.bar(
 )
 
 plt.ylabel("Promedio")
-plt.xlabel("Columnas")
+plt.xlabel("Calificaciones")
 plt.show()
